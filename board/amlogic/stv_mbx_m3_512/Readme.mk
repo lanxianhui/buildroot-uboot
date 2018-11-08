3acd6aa6ca696f9d02ae476de774c3c19447a9cd
lanxianhui <lanxianhui@126.com>
2018-11-08 10:33:29 +0800

    Update Readme.mk
    fix cpu

--- board/amlogic/stv_mbx_m3_512/Readme.mk
+++ board/amlogic/stv_mbx_m3_512/Readme.mk
@@ -1,5 +1,5 @@
 stv_mbx_m3_512_config: unconfig
-	$(MKCONFIG) $(@:_config=)  arm aml_meson stv_mbx_m3_512 amlogic m1
+	$(MKCONFIG) $(@:_config=)  arm aml_meson stv_mbx_m3_512 amlogic m3
 stv_mbx_m3_512_config_help:
 	@echo =======================================================================
 	@echo The mark in board is "STV MediaBox config for GENIATECH M3"
